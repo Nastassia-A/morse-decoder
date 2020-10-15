65c89df39f5cffd711cd32d9837bb25b1e73b5eb
Nastassia-A <nastassiaarkharova@gmail.com>
2020-10-15 15:38:17 +0300

    feat: implement decode function

--- src/index.js
+++ src/index.js
@@ -38,7 +38,27 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr = [];
+    let newStr = '';
+    for (let i = 0; i < expr.length; i += 10) {
+        arr.push(expr.slice(i, i+10));
+    }
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] === '**********') {
+        newStr += ' ';
+      } else {
+        let newLetter = '';
+        for (let j = 0; j < 10; j += 2) {
+          if (`${arr[i][j]}${arr[i][j + 1]}` === '10') {
+            newLetter += '.';
+          } else if (`${arr[i][j]}${arr[i][j + 1]}` === '11') {
+            newLetter += '-';
+          }
+        }
+        newStr += MORSE_TABLE[newLetter];
+      }
+    }
+    return newStr;
 }
 
 module.exports = {
